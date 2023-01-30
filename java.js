65d40b09f65b637056272b16a609866ccd3513f4
JoaoPedro-dot <castro.joa@northeastern.edu>
2023-01-30 16:24:55 -0500

    ic-05 final changes

---
+++ java.js
@@ -0,0 +1,21 @@
+// JS File for class demo
+// JP and Pratheek
+// Last modified 01.30.22
+
+//print
+console.log("hello world");
+
+// variables
+
+// constants
+const myCourse = "DS4200";
+console.log(myCourse);
+
+let season = "Winter"
+console.log(season);
+
+season = "Spring";
+console.log(season);
+
+
+
